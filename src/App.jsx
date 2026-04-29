0436d3057f50456a43c0c24b38c227ac0bd4a670
gtifaro <gabrieltifaro2021@gmail.com>
2026-04-28 21:05:02 -0500

    feat: Add play status and change board render

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState("X")
+  const updateBoard = (initialboard, initialTurn, cell) => {
+    let newBoard = [...initialboard]
+    newBoard[cell] = initialTurn
+    let newTurn = initialTurn == "X" ? "O" : "X"
+    setBoard(newBoard)
+    setTurn(newTurn)
+
+  }
+
+  const handleClick = (id) => {
+    updateBoard(board, turn, id)
+  };
 
   return (
-    <main className='tic_container'>
-      <h1 className='tic_title'>Tic Tac Toe</h1>
-      <div className='tic_board'>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>O</span>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>O</span>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>X</span>
-        <span className='board_cell'>O</span>
+    <main className="tic_container">
+      <h1 className="tic_title">Tic Tac Toe</h1>
+      <div className="tic_board">
+        {board.map((cell, idx) => {
+          return (
+            <span
+              key={`tic-cell-${idx}`}
+              onClick={() => handleClick(idx)}
+              className="board_cell"
+            >
+              {cell}
+            </span>
+          );
+        })}
       </div>
-      <h2 className='tic_nextTurn'>Es el turno de: X</h2>
-      <button className='tic_restart'>Reiniciar</button>
+      <h2 className="tic_nextTurn">Es el turno de: X</h2>
+      <button className="tic_restart">Reiniciar</button>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
